f7ac13ad6279cb63cadd69938dd28ceb34a0e233
Manas Nath <manasnath0902@gmail.com>
2026-07-04 22:08:08 +0100

    Public directory creation

--- scratch/find_js_tags.py
+++
@@ -1,7 +0,0 @@
-import re
-
-with open('d:/pet_proj/PetCircle/index.php', 'r', encoding='utf-8') as f:
-    text = f.read()
-
-scripts = re.findall(r'<script src="js/(.*?)"></script>', text)
-print(scripts)
